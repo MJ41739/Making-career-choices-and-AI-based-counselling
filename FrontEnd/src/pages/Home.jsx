f1d62863b56643e69f30bb2ddd774b2e16b2787f
MAYUR JADHAV <jadhavm45281@gmail.com>
2025-02-27 13:25:45 +0530

    ok

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate(); // Hook to get the navigate function
+
+  const gotodashboard = async () => {
+    navigate("/dashboard"); // Navigate to /test route
+  }
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <button type="submit" onClick={gotodashboard}>DashBoard</button>
+    </div>
+  );
 }
 
 export default Home
